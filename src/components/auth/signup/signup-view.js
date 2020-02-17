7b8209ad9b385775eaec591a3e9da078d504d6ae
Juan Fernandes <juan.jcf@outlook.com.br>
2020-02-17 19:25:31 -0300

    Added - Signup form

--- src/components/auth/signup/signup-view.js
+++ src/components/auth/signup/signup-view.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { Button } from "@blueprintjs/core";
+import { Card, Button, Elevation, FormGroup, InputGroup } from "@blueprintjs/core";
 import { redirect } from '../../../store/route';
+import { signUpAction } from '../../../store/auth';
+import { LoginViewBackground } from '../../../assets/images';
+
+const bgstyle = { display: 'flex', width: '100vw', height: '100vh', backgroundSize: 'contain', backgroundImage: `url(${ LoginViewBackground })` }
 
 export default function SignupView() {
   const dispatch = useDispatch();
+  const [validated, setValidated] = React.useState( false );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState( '' );
+  const [repeatPassword, setRepeatPassword] = useState( '' );
+
+  const handleSubmitForNewAccount = ( event ) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const invalid = !form.checkValidity();
+    const passwordMatch = password === repeatPassword;
+
+    if( !invalid && passwordMatch ) {
+      dispatch( signUpAction( username, password ) );
+      dispatch( redirect( 'product' ) );
+    }
+
+    setValidated( true );
+  };
 
   return (
-    <div>
-      <div>Signup View</div>
+    <div style={ bgstyle }>
+      <Card elevation={Elevation.ONE} style={{ width: '350px', margin: 'auto' }}>
+        <div>Create a new account</div>
 
-      <Button text="Login" onClick={ () => dispatch( redirect( 'login' ) ) } />
-      <Button text="Recover" onClick={ () => dispatch( redirect( 'recover' ) ) } />
+        <form noValidate onSubmit={ handleSubmitForNewAccount }>
+          <FormGroup labelFor="username-input" label="Username" labelInfo="(required)" intent="danger" helperText={ validated && !username ? 'This field is required' : ''  }>
+            <InputGroup id="username-input" leftIcon="user" placeholder="Password" value={ username } onChange={ event => setUsername( event.target.value ) } required />
+          </FormGroup>
+
+          <FormGroup labelFor="password-input" label="Password" labelInfo="(required)" intent="danger" helperText={ validated && !password ? 'This field is required' : ''  }>
+            <InputGroup id="password-input" leftIcon="lock" placeholder="Password" type="password" value={ password } onChange={ event => setPassword( event.target.value ) } required />
+          </FormGroup>
+
+          <FormGroup labelFor="password-repeat-input" label="Repeat password" labelInfo="(required)" intent="danger" helperText={ validated ? !repeatPassword ? 'This field is required' : repeatPassword !== password ? 'Password should match' : '' : ''  }>
+            <InputGroup id="password-repeat-input" leftIcon="lock" placeholder="Repeat password" type="password" value={ repeatPassword } onChange={ event => setRepeatPassword( event.target.value ) } required />
+          </FormGroup>
+
+          <Button type="submit" text="Create" />
+        </form>
+
+
+        <Button text="Login" onClick={ () => dispatch( redirect( 'login' ) ) } />
+        <Button text="Recover" onClick={ () => dispatch( redirect( 'recover' ) ) } />
+      </Card>
     </div>
   );
 }
--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -4,7 +4,6 @@ if( !auths ) localStorage.setItem( 'auths', JSON.stringify( [{ username: 'admin'
 
 //reducer
 const INITIAL_STATE = {
-  route: 'login',
   user: JSON.parse( localStorage.getItem( 'loggedUser' ) ),
 };
 
@@ -12,16 +11,37 @@ export default function AuthReducer(state = INITIAL_STATE, action) {
   const auths = JSON.parse( localStorage.getItem( 'auths' ) );
 
   switch (action.type) {
-    case 'LOG_IN':
-      const user = auths.find( auth => auth.username === action.user.username && auth.password === action.user.password );
+    case 'SIGN_IN':
+      var user = auths.find( auth => auth.username === action.user.username && auth.password === action.user.password );
       if( user ) localStorage.setItem( 'loggedUser', JSON.stringify({ username: user.username, password: user.password }) );
       return { ...state, user: user };
+
+    case 'SIGN_UP':
+      var user = action.user;
+      const auths = JSON.parse( localStorage.getItem( 'auths' ) );
+      auths.push( user );
+      localStorage.setItem( 'auths', JSON.stringify( auths ) );
+      localStorage.setItem( 'loggedUser', JSON.stringify({ username: user.username, password: user.password }) );
+      return { ...state, user: user };
+
+    case 'SIGN_OUT':
+        localStorage.setItem( 'loggedUser', '' );
+        return { ...state, user: undefined };
 
     default:
       return state;
   }
 };
 
-export const logInAction = ( user ) => {
-  return { type: 'LOG_IN', user: user };
+export const signInAction = ( user ) => {
+  return { type: 'SIGN_IN', user: user };
+};
+
+export const signOutAction = ( user ) => {
+  return { type: 'SIGN_OUT' };
+};
+
+export const signUpAction = ( username, password ) => {
+  const user = { username: username, password: password };
+  return { type: 'SIGN_UP', user: user };
 };
