d5c8a4b9059bd84421645012620810a96805a64a
Juan Fernandes <juan.jcf@outlook.com.br>
2020-02-19 13:17:35 -0300

    visual improvements

--- src/components/product/detail/product-detail-view.js
+++ src/components/product/detail/product-detail-view.js
@@ -7,7 +7,8 @@ import { redirect } from '../../../store/route';
 
 //default image
 import { SampleProductImage } from '../../../assets/image';
-const imgStyle = { width: '100vw', height: '40vh', objectFit: 'cover' };
+import { addProductToCartAction } from '../../../store/product';
+const imgStyle = { width: '100vw', height: '40vh', objectFit: 'contain' };
 
 export default function ProductDetailView() {
   const dispatch = useDispatch();
@@ -23,34 +24,43 @@ export default function ProductDetailView() {
   const price = product.price || '';
 
   const onAddToCart = () => {
-    
+    dispatch( addProductToCartAction( product.id ) );
   };
 
   const onBuyNow = () => {
-
+    if( !added ) dispatch( addProductToCartAction( product.id ) );
+    dispatch( redirect( 'shopping-cart' ) );
   };
 
   const renderAddToCartButton = () => {
     if( added ) return <Button disabled>Added</Button>;
-    return <Button className="bp3-minimal" icon="plus" text="Add to cart" onClick={ onAddToCart } />;
+    return <Button icon="plus" text="Add to cart" onClick={ onAddToCart } />;
   };
 
   return (
     <div>
       <Navbar className="bp3-dark">
         <Navbar.Group align={Alignment.LEFT}>
-          <Button className="bp3-minimal" icon="left-arrow" text="Back" onClick={ () => dispatch( redirect( 'shopping-cart' ) ) } />
+          <Button className="bp3-minimal" icon="arrow-left" text="Back to products" onClick={ () => dispatch( redirect( 'product-list' ) ) } />
         </Navbar.Group>
       </Navbar>
 
       <div>
         <img src={ SampleProductImage } style={ imgStyle } />
-        <div>{ title }</div>
-        <div>{ description }</div>
-        <div>{ price }</div>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-          { renderAddToCartButton() }
-          <Button className="bp3-minimal" icon="money" text="Buy Now" onClick={ onBuyNow } />
+        <div style={{ minWidth: '320px', maxWidth: '480px', padding: '10px 16px', margin: '0 auto', borderTop: '1px solid #e0e0e0' }}>
+          <div style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bold' }}>{ title }</div>
+
+          <div style={{ fontWeight: 'bold', margin: '8px 0' }}>Descriptions</div>
+          <div className="bp3-running-text" style={{ color: 'rgba( 0, 0, 0, 0.56 )' }}>{ description }</div>
+
+          <div style={{ display: 'flex', flexDirection: 'flex-row', margin: '8px 0', lineHeight: '30px' }}>
+            <div style={{ fontWeight: 'bold', flex: '1' }}>Price</div>
+            <div style={{ fontSize: '24px', fontWeight: 'bold' }}>${ ( price || 0 ).toFixed( 2 ) }</div>
+          </div>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            { renderAddToCartButton() }
+            <Button icon="shopping-cart" intent="primary" text="Buy Now" onClick={ onBuyNow } style={{ flex: '1', marginLeft: '10px' }} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/product/list/product-list-item.js
+++ src/components/product/list/product-list-item.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Card, Elevation } from "@blueprintjs/core";
+import { Card, Elevation, Colors } from "@blueprintjs/core";
 
 import { detailProductAction } from '../../../store/product';
 import { redirect } from '../../../store/route';
@@ -16,10 +16,12 @@ export default function ProductListItem({ id, title, filename, price }) {
   };
 
   return (
-    <Card elevation={Elevation.ONE} onClick={ onProductClicked } style={{ width: '350px', margin: '12px' }}>
-      <img src={ SampleProductImage } />
-      <div className="bp3-text-large" style={{ flex: '1' }}>{ title }</div>
-      <h5>{ price }</h5>
+    <Card elevation={Elevation.ONE} onClick={ onProductClicked } style={{ width: '300px', margin: '10px', padding: '0' }}>
+      <img src={ SampleProductImage } style={{ objectFit: 'contain', width: '100%' }} />
+      <div style={{ padding: '12px', borderTop: '1px solid #e0e0e0', background: '#f2f2f2' }}>
+        <div className="bp3-text-large" style={{ flex: '1', fontSize: '16px', fontWeight: 'bold' }}>{ title }</div>
+        <div style={{ fontSize: '16px', fontWeight: 'bold', color: 'rgba( 0, 0, 0, 0.56 )' }}>${ ( price ).toFixed( 2 ) }</div>
+      </div>
     </Card>
   );
 }
